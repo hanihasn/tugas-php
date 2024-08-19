f6ae093e288cf74434f3a1e193c1f31cf5837359
hani hasn <hanihasna85@gmail.com>
2024-08-19 14:59:18 +0700

    first commit

---
+++ input-usia.php
@@ -0,0 +1,67 @@
+<?php
+
+$usia = 10;
+
+if($usia >= 0 && $usia <= 12){
+    echo "Anak-anak";
+
+}else if($usia >= 13 && $usia <= 17){
+    echo "Remaja";
+
+}else if($usia >= 18 && $usia <= 59){
+    echo "Dewasa";
+
+}else {
+    echo "Lansia";
+}
+
+
+echo "<br>";
+$usia1 = 16;
+
+if($usia1 >= 0 && $usia1 <= 12){
+    echo "Anak-anak";
+
+}else if($usia1 >= 13 && $usia1 <= 17){
+    echo "Remaja";
+
+}else if($usia1 >= 18 && $usia1 <= 59){
+    echo "Dewasa";
+
+}else {
+    echo "Lansia";
+}
+
+
+echo "<br>";
+$usia2 = 20;
+
+if($usia2 >= 0 && $usia2 <= 12){
+    echo "Anak-anak";
+
+}else if($usia2 >= 13 && $usia2 <= 17){
+    echo "Remaja";
+
+}else if($usia2 >= 18 && $usia2 <= 59){
+    echo "Dewasa";
+
+}else {
+    echo "Lansia";
+}
+
+
+echo "<br>";
+$usia3 = 70;
+
+if($usia3 >= 0 && $usia3 <= 12){
+    echo "Anak-anak";
+
+}else if($usia3 >= 13 && $usia3 <= 17){
+    echo "Remaja";
+
+}else if($usia3 >= 18 && $usia3 <= 59){
+    echo "Dewasa";
+
+}else {
+    echo "Lansia";
+}
